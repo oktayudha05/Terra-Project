e36b630d2133666bcf74d7b18a6394413d544e3a
Oktario Mufti Yudha <oktayudha05@gmail.com>
2024-05-21 02:15:40 +0000

    move navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Navbar() {
     return (
         <nav>
-            <div className='flex item-center justify-center py-3 px-3'>
+            <div className='flex item-center justify-center py-1'>
                 <div className="flex-row-reverse space-x-1">
                     <Link href="/" className='hover:text-emerald-950 hover:bg-emerald-200 rounded px-2 py-1 text-emerald-100'>Home</Link>
                     <Link href="/about" className='hover:text-emerald-950  hover:bg-emerald-200 rounded px-2 py-1 text-emerald-100'>About</Link>
